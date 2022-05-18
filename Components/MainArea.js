9cde5f85b77f720b0ed7e60403425609d3e10bba
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-05-18 15:54:44 +0530

    icon

--- Components/MainArea.js
+++ Components/MainArea.js
@@ -1,11 +1,14 @@
 import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default class MainArea extends Component {
   render() {
     return (
-      <View>
-        <Text>MainArea</Text>
+      <View style={styles.Container}>
+        <Text style={styles.FruitText}>Fruits</Text>
+        <Text>View All</Text>
+        <AntDesign name="arrowright" style={styles.ArrowBtn} size={30} />
       </View>
     );
   }
