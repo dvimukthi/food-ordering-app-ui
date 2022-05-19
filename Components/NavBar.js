2f1d0eae68ab29f13357c43ea0e12b473143ad1e
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-05-19 16:50:50 +0530

    homeBtn

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,11 +1,14 @@
 import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
+import Octicons from "react-native-vector-icons/Octicons";
 
 export default class NavBar extends Component {
   render() {
     return (
-      <View>
-        <Text>NavBar</Text>
+      <View style={styles.Container}>
+        <View>
+          <Octicons name="home" style={styles.homeBtn} size={20} />
+        </View>
       </View>
     );
   }
