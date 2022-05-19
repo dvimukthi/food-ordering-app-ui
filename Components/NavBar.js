8d08884a962a20ebc4e1dce8782be74f9fc41963
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-05-19 16:53:18 +0530

    MailBtn

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -1,6 +1,7 @@
 import { Text, StyleSheet, View } from "react-native";
 import React, { Component } from "react";
 import Octicons from "react-native-vector-icons/Octicons";
+import Feather from "react-native-vector-icons/Feather";
 
 export default class NavBar extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class NavBar extends Component {
         <View>
           <Octicons name="home" style={styles.HomeBtn} size={20} />
           <Octicons name="search" style={styles.SearchBtn} size={20} />
+          <Feather name="mail" style={styles.MailBtn} size={20} />
         </View>
       </View>
     );
