e7c8608f4febb5e67475a4bb449a69c9be16114d
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-05-19 16:46:48 +0530

    NavBar

---
+++ Components/NavBar.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <View>
+        <Text>NavBar</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
